3905f84608acd591acf4dacfff15f4da961de21a
Paola Ramos Mendes <127321747+paolarmendes@users.noreply.github.com>
2026-04-21 11:47:44 -0500

    Add files via upload

---
+++ CH10 Assignment/script.js
@@ -0,0 +1,13 @@
+/*
+Paola Ramos Mendes
+04/10/2026
+script.js
+*/
+
+//Function to display content
+function content(){
+    var text = document.getElementById("new");
+    text.textContent="Hi. I am a new paragraph that was created with a JavaScript function.";
+    text.style.color = "#c0145f";
+    text.style.fontSize = "2em";
+}
